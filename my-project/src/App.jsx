1e6a6a8da48301fdd9dd10d489a9fced4ff38a4d
emmajjoseph <emmajoseph12345678@gmail.com>
2025-01-08 15:36:20 +0100

    Initial commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Projects from './components/Project'
+import Service from './components/Service'
+
+function App() {
+
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Service />
+        <Projects />
+        <Contact />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
